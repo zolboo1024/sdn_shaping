654738258b80587e7344158a5a87ff16a60b2568
skywalker007008 <skywalker007008@163.com>
2022-04-03 20:38:54 +0000

    add table_add

--- controller.py
+++ controller.py
@@ -1,26 +1,58 @@
-# Test
+#!/usr/bin/env python2
 
-from p4utils.utils.sswitch_p4runtime_API import SimpleSwitchP4RuntimeAPI
+from sswitch_CLI import SimpleSwitchAPI
+import runtime_CLI #as api
 
 # This is the test controller for this simulation
 # Not quite sure whether it can finally use in the test.
+
+# Build the CLI connection
+def build_runtime_CLI():
+	args = runtime_CLI.get_parser().parse_args()
+	args.pre = runtime_CLI.PreType.SimplePreLAG
+	services = runtime_CLI.RuntimeAPI.get_thrift_services(args.pre)
+	services.extend(SimpleSwitchAPI.get_thrift_services())
+
+	standard_client, mc_client, sswitch_client = runtime_CLI.thrift_connect(
+		args.thrift_ip, args.thrift_port, services
+	)
+	runtime_CLI.load_json_config(standard_client, args.json)
+	sswitch_cli = SimpleSwitchAPI(args.pre, standard_client, mc_client, sswitch_client)
+	return sswitch_cli
+
 class MyController:
-	def __init__(self, device_id=0, port=9090, ip='0.0.0.0', json_file=None):
-		self.device_id = device_id
-		self.port = port
-		self.ip = ip
-		self.json_file = json_file
-		self.controller = SimpleSwitchP4RuntimeAPI(
-			device_id = self.device_id,
-			grpc_port = self.port,
-			grpc_ip = self.ip,
-			json_path = self.json_file)
+	def __init__(self, sswitch_client):
+		self.client = sswitch_client
 
-	def start(self):
+	def connect(self):
+		self.client.cmdloop()
 
-		return 0
+	def table_add(self, table_name, action, keys, arguments):
+		# table_add table_name action keys [other_keys] => arguments [other_args]
+		line = table_name + " " + action + " "
+		for key in keys:
+			line += (key + " ")
+
+		line = line + "=> "
+		for arg in arguments:
+			line += (arg + " ")
+
+		self.client.do_table_add(line)
+
+	def table_add(self, line):
+		self.client.do_table_add(line)
+
+
+
+	def test_table_add(self):
+		line = "ipv4_lpm ipv4_forward 1 1 => 1"
+		self.client.do_table_add(line)
 
 if __name__ == "__main__":
-	controller = MyController(port = 9090)
-	controller.start()
+	
+	sswitch_cli = build_runtime_CLI()
+	controller = MyController(sswitch_cli);
+
+	#controller.connect()
+	controller.test_table_add()
 		
